7b9601962b6e5e936f3a039c50f413fe56765334
sharath <sharath9042@gmail.com>
2026-01-05 22:48:49 +0530

    added modulus floor division in simple calculator

--- arthimatic-operators/task01/simple_calculator.py
+++ arthimatic-operators/task01/simple_calculator.py
@@ -9,9 +9,18 @@ num2 = int(input("Enter the num2 value :"))
 addition = num1 + num2
 subtraction = num1 - num2
 multiplication = num1 * num2
-division = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+modulus = num1 % num2
+exponentitation = num1 ** num2
 
 print(num1,"and", num2, "addition value :", addition, 
       "subtraction value :",subtraction,
       "multiplication value :",multiplication, 
       "division value :",division)
+
+print(f"Floor division value {floor_division}")
+
+print(f"Modulus value {modulus}")
+
+print(f"Exponentiation value {exponentitation}") 
