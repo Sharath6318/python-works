817e7eafb6e3c8da5a902f9e26f536af87d94955
sharath <sharath9042@gmail.com>
2026-01-10 16:43:53 +0530

    added day 4 sime presentation file and jan 12 task

---
+++ day_04_semi.py
@@ -0,0 +1,30 @@
+# Logic_operator
+
+a, b, c = 10, 5, 15
+
+print(a > b and b < c)
+
+print(c > a or b > a)
+
+print(not(b > c))
+
+# identity operator
+
+x = [1, 2, 3]
+
+y = [1, 2, 3]
+
+z = x
+
+print(x is z)
+print(x is y)
+
+
+# Membership operator
+
+fruits = ["apple", "mango", "cherry"]
+
+print("cherry" in fruits)
+print("grape" not in fruits)
+
+
