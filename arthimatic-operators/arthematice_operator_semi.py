66e4d34f97b2f8306123c1ff4bde167737c528b7
sharath <sharath9042@gmail.com>
2026-01-16 23:21:07 +0530

    added logic, identity, membership oper presentation ex

--- arthimatic-operators/arthematice_operator_semi.py
+++ arthimatic-operators/arthematice_operator_semi.py
@@ -1,16 +1,49 @@
+# a = 10
+# b = 3
+
+# add = a + b
+# subtract = a - b
+# multiplication  = a * b
+# division = a / b
+# floor_division = a // b
+# modulus = a % b
+
+# print(f"addition {add}")
+# print(f"subtraction {subtract}")
+# print(f"multiplication {multiplication}")
+# print(f"division {division}")
+# print(f"floor division {floor_division}")
+# print(f"modulus {modulus}")
+
+
 a = 10
-b = 3
+b = 5
+c = 20
 
-add = a + b
-subtract = a - b
-multiplication  = a * b
-division = a / b
-floor_division = a // b
-modulus = a % b
+print(a > b and c > a)
 
-print(f"addition {add}")
-print(f"subtraction {subtract}")
-print(f"multiplication {multiplication}")
-print(f"division {division}")
-print(f"floor division {floor_division}")
-print(f"modulus {modulus}")
+print(a < c or b > a)
+
+print(not(c < b))
+
+
+# a = [1, 2, 3]
+# b = a
+
+# c = [2, 3, 4]
+# d = [2, 3, 4]
+
+# print(a is b)
+# print(c is d)
+
+lst = [10, 20, 30, 40, 50, 60]
+
+val = 40
+
+print(val in lst)
+
+print(70 not in lst)
+
+
+
+
